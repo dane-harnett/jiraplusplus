d223497672e0e6cb78e3678acb522c91044ca687
Dane Harnett <dane_harnett@flightcentre.com>
2012-11-06 11:17:52 +1000

    Initial files.

---
+++ main.js
@@ -0,0 +1,47 @@
+$(function(){
+	var defaults = {
+			customAssignees: {}
+		},
+		options = {};
+
+	if (typeof localStorage.jiraplusplus_options !== 'undefined') {
+		options = JSON.parse(localStorage.jiraplusplus_options);
+	}
+	if (typeof options !== 'object') {
+		options = {};
+	}
+	options = $.extend({}, defaults, options);
+
+	var specialAssigneeSelect = '<select name="specialAssigneeSelect" id="specialAssigneeSelect">';
+	specialAssigneeSelect += '<option value="">Custom Assignees...</option>';
+	$.each(options.customAssignees, function(i,v) {
+		specialAssigneeSelect += '<option value="'+v.email+'">'+v.name+'</option>';
+	});
+	specialAssigneeSelect += '</select>';
+
+	$(document).delegate('#specialAssigneeSelect', 'change', function(ev, el) {
+		$('#assignee').val($(this).val());
+		$('#assignee').trigger('liszt:updated');
+		ev.preventDefault();
+	});
+
+	var mainFunction = function() {
+
+		var $assigneeSelects = $('#assignee');
+
+		$.each($assigneeSelects, function(i,v) {
+			var $el = $(v);
+			
+			if ($el.parent().find('#specialAssigneeSelect').length == 0) {
+				$el.after(specialAssigneeSelect);
+			}
+			if (!$el.hasClass('chzn-done')) {
+				$el.chosen();
+				$el.parent().find('.chzn-drop .chzn-search input[type="text"]').focus();
+			}
+		});
+
+		setTimeout(mainFunction, 500);
+	};
+	setTimeout(mainFunction, 500);
+});
